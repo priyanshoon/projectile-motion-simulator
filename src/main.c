96b21b04a5a2a4b26a77c044dcb4c45e364e3df9
Priyanshu Prasad Gupta <guptapriyanshu973@gmail.com>
2023-09-19 13:33:40 +0530

    line movement

--- src/main.c
+++ src/main.c
@@ -3,30 +3,44 @@
 #include <stdlib.h>
 #include <raylib.h>
 #include "raygui.h"
+#include "projectile.h"
 
 int main(void) {
-  const int screenWidth = 900;
-  const int screenHeight = 500;
-  int height = 100;
+    const int screenWidth = 900;
+    const int screenHeight = 500;
 
-  InitWindow(screenWidth, screenHeight, "Projectile Motion Simulator");
 
-  SetTargetFPS(60);
+    InitWindow(screenWidth, screenHeight, "Projectile Motion Simulator");
 
-  while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(BLACK);
-    DrawText("Projectile Motion Simulator", 100, 10, 25, WHITE);
-    DrawLine(590, 0, 590, GetScreenHeight(), Fade(LIGHTGRAY, 0.6f));
-    DrawRectangle(590, 0, GetScreenWidth() - 500, GetScreenHeight(), Fade(DARKGRAY, 0.3f));
-    GuiSliderBar((Rectangle){ 700, 40, 130, 20 }, "Height", NULL, 100, -400, 400);
-    GuiSliderBar((Rectangle){ 700, 70, 130, 20 }, "Initial Speed", NULL, 0, -400, 400);
-    GuiSliderBar((Rectangle){ 700, 100, 130, 20 }, "Range", NULL, 100, -400, 400);
-    GuiButton( (Rectangle){ 700, 150, 130, 40 }, "Launch" );
-    EndDrawing();
-  }
+    int height = 100;
+    float startAngle = 400.0f;
 
-  CloseWindow();
+    SetTargetFPS(60);
 
-  return 0;
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        DrawText("Projectile Motion Simulator", 100, 10, 25, WHITE);
+        DrawLine(590, 0, 590, GetScreenHeight(), Fade(LIGHTGRAY, 0.6f));
+        DrawRectangle(590, 0, GetScreenWidth() - 500, GetScreenHeight(), Fade(DARKGRAY, 0.3f));
+        DrawLine(0, 400, 590, 400, WHITE);
+        DrawLine(100, 400, 400, startAngle, GREEN);
+        DrawCircle(100, 400, 15.0, RED);
+
+        startAngle = GuiSliderBar((Rectangle){ 700, 40, 130, 20 }, "Angle", NULL, startAngle, 0, 400);
+        GuiSliderBar((Rectangle){ 700, 70, 130, 20 }, "Height", NULL, 100, -400, 400);
+        GuiSliderBar((Rectangle){ 700, 100, 130, 20 }, "Initial Speed", NULL, 0, -400, 400);
+
+        if (GuiButton( (Rectangle){ 700, 200, 130, 40 }, "Launch" )) {
+            printf("yooo!\n");
+        }
+
+        DrawFPS(10, 10);
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
--- src/projectile.h
+++ src/projectile.h
@@ -1,3 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+const float g = 10;
+
+float getAngle(int x, int y) {
+}
+
